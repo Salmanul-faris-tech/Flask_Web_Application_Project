4c6b68c729727bd26bd3b111ae868ac66c175c57
Salmanul Faris <116569615+Salmanul-faris-tech@users.noreply.github.com>
2026-02-06 15:17:05 +0530

    Update app.py

--- app.py
+++ app.py
@@ -43,9 +43,9 @@ app.config["MAIL_PORT"] = 587
 app.config["MAIL_USE_TLS"] = True
 app.config["MAIL_USE_SSL"] = False
 
-app.config["MAIL_USERNAME"] = "salmanbughunter@gmail.com"
-app.config["MAIL_PASSWORD"] = "zkttvyxhcfkkityg"
-app.config["MAIL_DEFAULT_SENDER"] = "salmanbughunter@gmail.com"
+app.config["MAIL_USERNAME"] = "your_mail@gmail.com"
+app.config["MAIL_PASSWORD"] = "you_app_password"
+app.config["MAIL_DEFAULT_SENDER"] = "your_mail@gmail.com"
 
 mail = Mail(app)
 serializer = URLSafeTimedSerializer(app.config["SECRET_KEY"])
